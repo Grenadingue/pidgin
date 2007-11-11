0b31983d7dd9c17f6f683a4cbb7690baf1bba25a
Will Thompson <resiak@pidgin.im>
2007-11-11 14:19:55 +0000

    propagate from branch 'im.pidgin.pidgin' (head 86eea945f0ace141eeaf591895806942d9a538ca)             to branch 'im.pidgin.cpw.resiak.disconnectreason' (head 0c78851f576b8cb68de872bdb3a4e59041cb12cc)

--- libpurple/protocols/sametime/sametime.c
+++ libpurple/protocols/sametime/sametime.c
@@ -1738,11 +1738,12 @@ static void read_cb(gpointer data, gint source, PurpleInputCondition cond) {
                                    msg);
 
   } else if(ret < 0) {
-    char *msg = g_strerror(err);
+    const gchar *err_str = g_strerror(err);
+    char *msg = NULL;
 
-    DEBUG_INFO("error in read callback: %s\n", msg);
+    DEBUG_INFO("error in read callback: %s\n", err_str);
 
-    msg = g_strdup_printf(_("Error reading from socket: %s"), msg);
+    msg = g_strdup_printf(_("Error reading from socket: %s"), err_str);
     purple_connection_error_reason(pd->gc,
                                    PURPLE_CONNECTION_ERROR_NETWORK_ERROR,
                                    msg);
